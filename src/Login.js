e3a9474b0bf62e6bf52842dd51d6fd2fb21157d6
yeabtsega45 <yabtsega2022@gmail.com>
2022-12-22 17:05:12 +0300

    useState hook on login page

--- src/Login.js
+++ src/Login.js
@@ -1,19 +1,47 @@
 import React from "react";
 import "./Login.css";
 import logo from "./assets/logo2.png";
+import { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
     <div className="login">
       <div className="header__title">
         <img src={logo} alt="logo" />
         <h2>Yishak Nutrition site</h2>
       </div>
-      <form>
-        <input placeholder="full name" type="text" />
-        <input placeholder="email" type="text" />
-        <input placeholder="password" type="password" />
-        <button>Sign In</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          placeholder="email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          onClick={(e) => console.info({ name, email, password })}
+          type="submit"
+        >
+          Sign In
+        </button>
       </form>
       <p>Not a member?</p>
       <button>Register</button>
