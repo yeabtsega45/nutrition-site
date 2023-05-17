97236f364a29f0911abae4976833080913cdda44
yeabtsega45 <yabtsega2022@gmail.com>
2023-05-18 00:08:51 +0300

    working on contact page

--- src/Contact.js
+++ src/Contact.js
@@ -7,20 +7,22 @@ function Contact() {
     <div className="contact">
       <img src={img2} alt="fruit" />
       <h1>Contact Us</h1>
-      <p>
-        I am an experienced Registered Dietitians and nutrition experts who work
-        at --------- as Nutrition Information Specialists.
-      </p>
-      <p>
-        I am available to answer food and nutrition questions. Questions and
-        comments can be provided via email and you will receive a response to
-        your inquiry.
-      </p>
-      <p className="bold">Phone</p>
-      <p>+251 9--------</p>
-      <p className="bold">Address</p>
-      <p>Ethiopia , Addis Ababa</p>
-      <p>Yeka subcity, Kotebe</p>
+      <div className="contact__text">
+        <p>
+          I am an experienced Registered Dietitians and nutrition experts who
+          work at --------- as Nutrition Information Specialists.
+        </p>
+        <p>
+          I am available to answer food and nutrition questions. Questions and
+          comments can be provided via email and you will receive a response to
+          your inquiry.
+        </p>
+        <p className="bold">Phone</p>
+        <p>+251 9--------</p>
+        <p className="bold">Address</p>
+        <p>Ethiopia , Addis Ababa</p>
+        <p>Yeka subcity, Kotebe</p>
+      </div>
     </div>
   );
 }
